85ec956a972296920c68d59ff397230a0340a919
Oyerinde Samuel <oyerindesamuelabiodun@gmail.com>
2022-09-07 13:23:36 +0100

    hover effect

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,27 +1,54 @@
-import './project.scss';
+import './project.scss'
 import Loader from 'react-loaders'
-import Greenbrain from '../../assets/images/greenbrain.png';
-import Paybetta from '../../assets/images/paybetta.png';
-import Photodesign from '../../assets/images/photodesign.png';
-import Uranno from '../../assets/images/uranno-png.png';
+import Greenbrain from '../../assets/images/greenbrain.png'
+import Paybeta from '../../assets/images/paybettta.png'
+import Photodesign from '../../assets/images/photodesign.png'
+import Uranno from '../../assets/images/uranno-png.png'
+
+
 const Project = () => {
-    return (
-        <>
-            <div className="container project-page">
-                <div className="image-page">
-                    <img src={Greenbrain} alt="greenbrain projec" className="img-1" />
-                    <br />
-                    <img src={Paybetta} alt="paybetta projec" className="img-2" />
-                </div>
-                <div className="image-page">
-                    <img src={Photodesign} alt="photodesign projec" className="img-3" />
-                    <br />
-                    <img src={Uranno} alt="uranno projec" className="img-4" />
-                </div>
-                
+  return (
+    <>
+      <div className="container project-page">
+        <div className="image-page page1">
+          <div className="image-con">
+            <img src={Greenbrain} alt="greenbrain projec" className="img-1" />
+            <div className="middle">
+              <a href="greenbrain.netlify.app" className="img-link">
+                Greenbrain Project
+              </a>
             </div>
-            <Loader type="pacman" />
-        </>
-    )
+          </div>
+          <div className="image-con">
+            <img src={Uranno} alt="uranno projec" className="img-2" />
+            <div className="middle">
+              <a href="uranno.netlify.app" className="img-link">
+                Uranno Project
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="image-page">
+          <div className="image-con">
+            <img src={Photodesign} alt="photodesign projec" className="img-3" />
+            <div className="middle">
+              <a href="photography.netlify.app" className="img-link">
+                Photography Project
+              </a>
+            </div>
+          </div>
+          <div className="image-con">
+            <img src={Paybeta} alt="paybetta projec" className="img-4" />
+            <div className="middle">
+              <a href="paybetta.netlify.app" className="img-link">
+                Paybetta Project
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 export default Project
